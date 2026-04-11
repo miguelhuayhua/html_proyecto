4e79743de8400584094bc6dbf379b2d1f0fc7cb4
Miguel Huayhua <miguelhuayhuac@gmail.com>
2026-04-10 22:43:37 -0400

    clase 7

--- index.js
+++ index.js
@@ -1,68 +1,46 @@
 
 
-
-let jugadores = [
-    {
-        nombre: "Mark Thompson",
-        nacimiento: "Enero 12, 1993",
-        tipo: "Delantero",
-        fotografia: "https://dominio.com/jugadores/1.png"
-    },
-    {
-        nombre: "Mike Jhonson",
-        nacimiento: "Enero 12, 1993",
-        tipo: "Delantero",
-        fotografia: "https://dominio.com/jugadores/1.png"
-    },
-    {
-        nombre: "Mark Thompson",
-        nacimiento: "Enero 12, 1993",
-        tipo: "Delantero",
-        fotografia: "https://dominio.com/jugadores/1.png"
-    },
-    {
-        nombre: "Mark Thompson",
-        nacimiento: "Enero 12, 1993",
-        tipo: "Delantero",
-        fotografia: "https://dominio.com/jugadores/1.png"
-    }
-
-]
-
-
-let partidos = [
-    {
-        estadium: "Bernaveu",
-        fecha: "12 de Octubre",
-        equipos: [
-            { logo: "https://url.com/2.png", nombre: "Real Madrid" },
-            { logo: "https://url.com/4.png", nombre: "Sevilla FC" }]
-    },
-    {
-        estadium: "Bernaveu",
-        fecha: "12 de Octubre",
-        equipos: [
-            { logo: "https://url.com/2.png", nombre: "Real Madrid" },
-            { logo: "https://url.com/4.png", nombre: "Sevilla FC" }]
-    }, {
-        estadium: "Bernaveu",
-        fecha: "12 de Octubre",
-        equipos: [
-            { logo: "https://url.com/2.png", nombre: "Real Madrid" },
-            { logo: "https://url.com/4.png", nombre: "Sevilla FC" }]
-    }
-]
-
 const contenedorDatos = document.getElementById("contenedor");
 
-jugadores.forEach(function (jugador) {
-    console.log(jugador)
-    let tarjeta = document.createElement("div");
-    tarjeta.textContent = jugador.nombre;
-    tarjeta.className = "bg-red-400 text-white border mt-4"
-    tarjeta.classList.add("p-7")
-    contenedorDatos.appendChild(tarjeta)
+
+
+fetch('https://ysmgcapyfdzdpagzkcgo.supabase.co/functions/v1/clase_html_api', {
+    method: 'POST',
+    headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer sb_publishable_3WXE382vKEFyMmhAYwbTpA_lP80irOs",
+    }
+}).then(respuesta => {
+    return respuesta.json();
+}).then(datos => {
+
+    let { jugadores, partidos } = datos;
+    jugadores.forEach(function (jugador) {
+        let tarjeta = document.createElement("div");
+        tarjeta.textContent = jugador.nombre;
+        tarjeta.className = "bg-red-400 text-white border mt-4"
+        tarjeta.classList.add("p-7")
+        contenedorDatos.appendChild(tarjeta)
+    })
+
+
 })
 
 
-fetch("https://dominio.com/jugadores")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
