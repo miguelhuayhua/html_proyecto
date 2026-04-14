daf68033ef2e5e0dd8a15ef295335a7c7bef72b3
Miguel Huayhua <miguelhuayhuac@gmail.com>
2026-04-13 22:36:55 -0400

    version 3, clase 13 de Abril - Lunes

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 
 
 const contenedorDatos = document.getElementById("contenedor");
-
-
+const contenedorPartidos = document.getElementById("contenedor-partidos");
 
 fetch('https://ysmgcapyfdzdpagzkcgo.supabase.co/functions/v1/clase_html_api', {
     method: 'POST',
@@ -15,13 +14,81 @@ fetch('https://ysmgcapyfdzdpagzkcgo.supabase.co/functions/v1/clase_html_api', {
 }).then(datos => {
 
     let { jugadores, partidos } = datos;
-    jugadores.forEach(function (jugador) {
+    //creación de los jugadores
+    jugadores.splice(0, 4).forEach(function (jugador) {
         let tarjeta = document.createElement("div");
-        tarjeta.textContent = jugador.nombre;
-        tarjeta.className = "bg-red-400 text-white border mt-4"
-        tarjeta.classList.add("p-7")
+        let img = document.createElement("img");
+        let h3 = document.createElement("h3");
+        let p = document.createElement("p");
+
+        //asignación de información
+        img.alt = "Imagen de :" + jugador.nombre;
+        img.src = jugador.fotografia;
+        h3.textContent = jugador.nombre;
+        p.textContent = jugador.nacimiento;
+
+        //asignación de estilos
+
+        tarjeta.className = "flex rounded-lg overflow-hidden flex-col bg-white"
+        h3.className = "font-bold"
+
+        //asignación de componentes al padre (tarjeta)
+        tarjeta.appendChild(img);
+        tarjeta.appendChild(h3);
+        tarjeta.appendChild(p);
         contenedorDatos.appendChild(tarjeta)
     })
+
+
+
+    //creación de partidos
+
+
+    partidos.forEach(partido => {
+        const { equipos, estadium, fecha } = partido;
+        const tarjeta2 = document.createElement('div');
+
+        const equipoOficial = document.createElement('div');
+
+        const equipoRival = document.createElement('div');
+        const acciones = document.createElement('div');
+
+
+        const info_oficial = equipos.at(0);
+        const info_rival = equipos.at(1);
+        //LLENADO DE INFORMACIÓN
+        let imagenOficial = document.createElement('img');
+        let nombreOficial = document.createElement('span');
+        imagenOficial.src = info_oficial.logo;
+        imagenOficial.className = "size-20"
+        nombreOficial.textContent = info_oficial.nombre;
+        nombreOficial.className = "font-bold"
+        equipoOficial.appendChild(imagenOficial);
+        equipoOficial.appendChild(nombreOficial);
+        equipoOficial.className = " flex items-center gap-5 col-span-2"
+
+        tarjeta2.className = "grid grid-cols-5 rounded-xl hover:bg-gray-100 p-6 shadow bg-white";
+        tarjeta2.appendChild(equipoOficial);
+
+        contenedorPartidos.appendChild(tarjeta2)
+    })
+
+})
+
+
+
+const formulario = document.getElementById("formulario");
+
+
+formulario.addEventListener("submit", (evento) => {
+    //importante
+    evento.preventDefault();
+    //lógica propia 
+    let datos = new FormData(formulario);
+    let mensaje = datos.get('mensaje')
+    let nombre = datos.get('nombre');
+    console.log("Mi mensaje: " , mensaje)
+    console.log("Mi nombre: " , nombre)
 
 
 })
@@ -31,8 +98,10 @@ fetch('https://ysmgcapyfdzdpagzkcgo.supabase.co/functions/v1/clase_html_api', {
 
 
 
-
-
+const form = document.getElementById('form')
+form.addEventListener('submit', function (ev) {
+    ev.preventDefault()
+})
 
 
 
